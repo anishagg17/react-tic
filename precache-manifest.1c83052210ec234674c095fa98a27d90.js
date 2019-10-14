f7255847bc4dd6c258ddd2aeabb59322cc79ba73
Anish Aggarwal <anish17122000@gmail.com>
2019-10-14 22:23:20 +0530

    Updates

---
+++ precache-manifest.1c83052210ec234674c095fa98a27d90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7debe69541fa576e51aaea1f071fa299",
+    "url": "/react-tic/index.html"
+  },
+  {
+    "revision": "8e6c1536e745584fa72d",
+    "url": "/react-tic/static/css/main.82fa0d8b.chunk.css"
+  },
+  {
+    "revision": "0bf8141fd2551fe3ce1a",
+    "url": "/react-tic/static/js/2.02b256f4.chunk.js"
+  },
+  {
+    "revision": "8e6c1536e745584fa72d",
+    "url": "/react-tic/static/js/main.381e40e8.chunk.js"
+  },
+  {
+    "revision": "692e229da1ca665a7a44",
+    "url": "/react-tic/static/js/runtime-main.dcc8ed9e.js"
+  }
+]);
